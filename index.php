6077ed8385aaf29c132f27574273d26f2041954d
Noah Weiner <noah.weiner@yale.edu>
2021-02-24 02:05:25 -0500

    install tw, start php scripting

--- index.php
+++ index.php
@@ -1,8 +1,12 @@
 <html>
 	<head>
-		<title>Welcome to shenandoahtheprius.com!</title>
+		<title>Welcome to indygopunctual.com!</title>
+		<link href="css/output.css" type="text/css" rel="stylesheet">
+		<link href="css/home.css" type="text/css" rel="stylesheet">
 	</head>
 	<body>
-		<?php echo '<p>Hello World!</p>'; ?>
+		
+		<?php echo '<p class="text-6xl">Welcome to indygopunctual.com!</p>'; ?>
+		<p class="text-6xl italic"></p>
 	</body>
 </html>
